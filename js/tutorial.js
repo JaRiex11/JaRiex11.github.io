b3abc441344aa38ee58b1606dca0621519b2d5ea
JaRiex11 <bolotov3sta@gmail.com>
2024-05-31 00:38:32 +0800

    Сделал хотьбу, и покадровую анимацию атаки(но она перестала работать перед самым сохранением, завтра найду ошибку)

--- js/tutorial.js
+++ js/tutorial.js
@@ -1,66 +1,183 @@
-var ctx, canvas;
+const SCALE = 2;
+const WIDTH = 50;
+const HEIGHT = 45;
+const SCALED_WIDTH = SCALE * WIDTH;
+const SCALED_HEIGHT = SCALE * HEIGHT;
+const MOVEMENT_SPEED = 5;
+const FRAME_X = 314;
+const FRAME_Y = 340;
+
+let canvas;
+let ctx;
+let keyPresses = {};
+let mousePresses = {};
+let mouseX, mouseY;
+let lockAnimation = false;
+let pl;
+
+class player {
+    positionX = 0;
+    positionY = 0;
+    playerImageX = FRAME_X;
+    playerImageY = FRAME_Y;
+
+    attackAnimation(frameCount) {
+        if (frameCount == 5) {
+            playerImageY = 745;
+            frameCount++;
+            if (lockAnimation) {
+                setTimeout(attackAnimation, 70, frameCount);
+            }
+        }
+        if (frameCount > 5) {
+            frameCount = 0;
+            playerImageX = FRAME_X;
+            playerImageY = FRAME_Y;
+            lockAnimation = false;
+            return;
+        }
+        playerImageX = 314;
+        playerImageY = 699 + (45 * frameCount);
+
+        frameCount++;
+        if (lockAnimation) {
+            setTimeout(attackAnimation, 70, frameCount);
+        }
+
+    }
+}
+
 function init() {
     canvas = document.getElementById("canvas");
-    canvas.width = 500;
-    canvas.height = 500;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    canvas.addEventListener('mousemove', function (evt) {
+        var rect = this.getBoundingClientRect();
+
+        mouseX = evt.clientX - rect.left;
+        mouseY = evt.clientY - rect.top;
+        //console.log(mouseX + ", sss" + mouseY);
+
+    }, false);
+
+    pl = new player();
 
     ctx = canvas.getContext('2d');
 }
 
 let img = new Image();
-img.src = '../img/PC Computer - Hotline Miami 2 Wrong Number - Police.png';
-img.onload = function () {
-    window.requestAnimationFrame(gameLoop);
-};
-
-const SCALE = 2;
-const WIDTH = 56;
-const HEIGHT = 58;
-const SCALED_WIDTH = SCALE * WIDTH;
-const SCALED_HEIGHT = SCALE * HEIGHT;
-function drawFrame(frameX, frameY, canvasX, canvasY) {
-    ctx.drawImage(img,
-        frameX * WIDTH, frameY * HEIGHT, WIDTH, HEIGHT,
-        canvasX, canvasY, SCALED_WIDTH, SCALED_HEIGHT);
-}
-const CYCLE_LOOP = [0, 1, 0, 2];
-let currentLoopIndex = 0;
-let frameCount = 0;
-let currentDirection = 0;
-
-let keyPresses = {};
-window.addEventListener('keydown', keyDownListener, false);
+window.addEventListener('keydown', keyDownListener);
 function keyDownListener(event) {
     keyPresses[event.key] = true;
 }
-window.addEventListener('keyup', keyUpListener, false);
+window.addEventListener('keyup', keyUpListener);
 function keyUpListener(event) {
     keyPresses[event.key] = false;
 }
 
-const MOVEMENT_SPEED = 3;
-let positionX = 0;
-let positionY = 0;
+window.addEventListener('mousedown', mouseDownListener);
+function mouseDownListener(event) {
+    if (event.button == 0 && !lockAnimation) { //left mouse button
+        console.log("hf,jnftn");
+        lockAnimation = true;
+        if (lockAnimation) {
+            pl.attackAnimation(0);
+        }
+
+    }
+    mousePresses[event.button] = true;
+}
+window.addEventListener('mouseup', mouseUpListener);
+function mouseUpListener(event) {
+    mousePresses[event.button] = false;
+}
+
+function loadImage() {
+    img.src = '../img/PC Computer - Hotline Miami 2 Wrong Number - Police.png';
+    img.onload = function () {
+        window.requestAnimationFrame(gameLoop);
+    };
+}
+function drawFrame(frameX, frameY, canvasX, canvasY) {
+    ctx.drawImage(img,
+        frameX/* * WIDTH*/, frameY/* * HEIGHT*/, WIDTH, HEIGHT,
+        canvasX, canvasY, SCALED_WIDTH, SCALED_HEIGHT);
+}
+
+loadImage();
+
 function gameLoop() {
+    mX = mouseX - pl.positionX - WIDTH / 2.0;
+    mY = mouseY - pl.positionY - HEIGHT / 2.0 - 10;
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    if (keyPresses.w && keyPresses.s) {
-        positionY -= 0;
-    } else if (keyPresses.w) {
-        positionY -= MOVEMENT_SPEED;
-        //currentDirection = FACING_UP;
+    if (keyPresses.w) {
+        moveCharacter(0, -MOVEMENT_SPEED);
     } else if (keyPresses.s) {
-        positionY += MOVEMENT_SPEED;
-        //currentDirection = FACING_DOWN;
+        moveCharacter(0, MOVEMENT_SPEED);
     }
-    if (keyPresses.a && keyPresses.d) {
-        positionX -= 0;
-    } else if (keyPresses.a) {
-        positionX -= MOVEMENT_SPEED;
-        //currentDirection = FACING_LEFT;
+    if (keyPresses.a) {
+        moveCharacter(-MOVEMENT_SPEED, 0);
     } else if (keyPresses.d) {
-        positionX += MOVEMENT_SPEED;
-        //currentDirection = FACING_RIGHT;
+        moveCharacter(MOVEMENT_SPEED, 0);
     }
-    drawFrame(0, currentDirection, positionX, positionY);
+
+    ctx.save();
+    ctx.fillRect(pl.positionX + WIDTH / 2.0, pl.positionY + HEIGHT / 2.0, 10, 10)
+    //квадратик для центра
+
+    ctx.translate(pl.positionX + WIDTH / 2.0, pl.positionY + HEIGHT / 2.0);
+    ctx.translate(8, 8);
+    var asin = Math.asin(mY / Math.sqrt(mY * mY + mX * mX));
+    var acos = Math.acos(mX / Math.sqrt(mY * mY + mX * mX));
+    var rotation = asin;
+
+    if (acos > Math.PI / 2.) {
+        rotation = -Math.PI - asin;
+    }
+    ctx.rotate(rotation);
+    ctx.translate(-8, -8);
+    drawFrame(pl.playerImageX, pl.playerImageY, -30, -35);
+    ctx.translate(0, 0);
+    ctx.restore();
     window.requestAnimationFrame(gameLoop);
 }
+
+function moveCharacter(deltaX, deltaY) {
+    /*console.log("cvs width" + canvas.width)
+    console.log("positionX = " + positionX)*/
+    if (pl.positionX + deltaX > -WIDTH / 2.0 && pl.positionX + SCALED_WIDTH + deltaX < canvas.width) {
+        pl.positionX += deltaX;
+    }
+    if (pl.positionY + deltaY > -HEIGHT / 2.0 && pl.positionY + SCALED_HEIGHT + deltaY < canvas.height) {
+        pl.positionY += deltaY;
+    }
+}
+
+/*function attackAnimation(frameCount) {
+    if (frameCount == 5) {
+        playerImageY = 745;
+        frameCount++;
+        if (lockAnimation) {
+            setTimeout(attackAnimation, 70, frameCount);
+        }
+    }
+    if (frameCount > 5) {
+        frameCount = 0;
+        playerImageX = FRAME_X;
+        playerImageY = FRAME_Y;
+        lockAnimation = false;
+        return;
+    }
+    playerImageX = 314;
+    playerImageY = 699 + (45 * frameCount);
+
+    frameCount++;
+    if (lockAnimation) {
+        setTimeout(attackAnimation, 70, frameCount);
+    }
+
+}*/
+
+//30 на 16 размеры
+//(34, 27) коорды
